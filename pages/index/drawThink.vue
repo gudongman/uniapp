56b7c3dd529c4311b18c83b5c806a03bb2c6b7ba
xu_xiao_qian <5328665+xu_xiao_qian@user.noreply.gitee.com>
2021-03-03 15:15:39 +0800

    1

--- pages/index/drawThink.vue
+++ pages/index/drawThink.vue
@@ -4,7 +4,7 @@
 		
 		1.计算权重的和；Math.random() * 权重和 为随机值 
 		2.将随机值放入数组，从小到大排序（sort）,找到随机值对应的下标 ；且下标不大于（数组长度-1） 
-		3.原数组的 this.list[randomIndex] 为抽中的奖项 
+		3.原数组的 this.list[randomIndex] 为抽中的奖项   
 		
 		<u-divider>动画创建的代码 </u-divider>
 		var animationRun = uni.createAnimation({
